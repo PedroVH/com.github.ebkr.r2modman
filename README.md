681dfc20b2136ac678ce5c47deff744c67463495
Ashley Wulber <ashley@system76.com>
2022-06-18 16:04:14 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+	<img src="com.github.ebkr.r2modman.png" width="200px"><br>
+</p>
+
+# r2modman
+
+-Support for Risk of Rain 2, Dyson Sphere Program, Valheim, GTFO, Outward, TaleSpire, H3VR, ROUNDS, Mechanica, Muck, BONEWORKS, Lethal League Blaze, Timberborn, Totally Accurate Battle Simulator, Nickelodeon All-Star Brawl, Inscryption, Starsand, and more.
+-A clean user interface designed to make modding as simple as possible.
+-Safer mod installation allowing you to play the game through Steam normally.
+-Mod profiles to switch between different sets of mods quickly and easily.
+-Export profiles to easily share both your mods and configs with friends.
+-Download and install mods directly from the manager.
+-View and update any outdated mods.
+-Edit configs directly from the manager.
+-Manager auto-updates.
+-And more!
+
+
+## Flatpak specific issues
---
+++ update-version.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+- Checks Github API for newest version of r2modman
+- Downloads it for checksum calculation 
+- Updates Flatpak manifest accordingly
+"""
+# %%
+
+# Imports
+# Only modules in the standard library were chosen to avoid needing to install Python dependencies
+import hashlib
+import re
+import json
+import urllib.request
+
+# Pre-defined values
+REPO_API_RELEASE_URL = "https://github.com/ebkr/r2modmanPlus/releases/latest"
+FLATPACK_MANIFEST_NAME = "com.github.ebkr.r2modman.yaml"
+
+
+# Get JSON object containing info about all releases
+api_response = urllib.request.urlopen(REPO_API_RELEASE_URL).read()
+
+# Get version number of newest release
+release_version = json.loads(api_response)["tag_name"].replace("v", "")
+print(f"Newest version is: {release_version}")
+
+# The part of the JSON object that refers to the tar archive
+tar_object = [x for x in json.loads(
+    api_response)["assets"] if "tar.gz" in x["name"]][0]
+
+
+# Simple check to ensure that versions match up
+assert(release_version in tar_object["name"])
+
+# Download tar archive file for checksum calculation
+g = urllib.request.urlopen(tar_object["browser_download_url"])
+with open(tar_object["name"], "bw") as f:
+    f.write(g.read())
+
+# Calculate SHA256 checksum to update in manifest
+sha256_hash = hashlib.sha256()
+with open(tar_object["name"], "rb") as f:
+    # Read and update hash string value in blocks of 4K
+    for byte_block in iter(lambda: f.read(4096), b""):
+        sha256_hash.update(byte_block)
+    release_checksum = sha256_hash.hexdigest()
+
+
+# Read Flatpak manifest
+with open(FLATPACK_MANIFEST_NAME, "rt") as f:
+    file_content = f.read()
+
+# Set version number, size, and checksum
+file_content = re.sub(r"(\d+\.\d+\.\d+)", release_version, file_content)
+file_content = re.sub(r"size: \d+", f"size: {tar_object['size']}", file_content, 0, re.MULTILINE)
+file_content = re.sub(r"sha256: [0-9a-fA-F]+", f"sha256: {release_checksum}", file_content, 0, re.MULTILINE)
+
+# Write back updated content
+with open(FLATPACK_MANIFEST_NAME, "wt") as f:
+    f.write(file_content)
+
+print("Done")
+print("Don't forget to manually update the AppData XML (com.github.ebkr.r2modman.appdata.xml) !")
